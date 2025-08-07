45edafb608bca43af57608479da95f3b1ea7c66b
Randy Harkediansa <randy.harkediansa@gmail.com>
2025-08-07 13:46:22 +0700

    add const

---
+++ constants.ts
@@ -0,0 +1,13 @@
+
+export const CATEGORIES: string[] = [
+  'General Knowledge',
+  'Science & Nature',
+  'History',
+  'Geography',
+  'Movies',
+  'Music',
+  'Sports',
+  'Technology',
+  'Art & Literature',
+  'Food & Drink'
+];
